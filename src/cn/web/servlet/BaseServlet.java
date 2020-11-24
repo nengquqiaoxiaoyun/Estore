4a538fbf847c32b078fb58cb0ddc355d7d4baeb4
huakaimay <huakaimay@gmail.com>
2020-11-24 14:44:50 +0800

    通用servlet

---
+++ src/cn/web/servlet/BaseServlet.java
@@ -0,0 +1,30 @@
+package cn.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+/**
+ * @author: huakaimay
+ * @since: 2020-11-24
+ */
+public class BaseServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String methodName = request.getParameter("methodName");
+        try {
+            Method declaredMethod = this.getClass().getDeclaredMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
+            declaredMethod.setAccessible(true);
+            declaredMethod.invoke(this, request, response);
+        } catch (Exception e) {
+            throw new RuntimeException("服务器异常");
+        }
+    }
+}
